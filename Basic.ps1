1393acd301bec073c98e055909ec36a3df8e7ae3
M Umer Afzal <me.mumerafzal@gmail.com>
2024-04-08 02:15:00 -0700

    update

--- Basic.ps1
+++ Basic.ps1
@@ -1,1 +1,5 @@
 $PSVersionTable.PSVersion
+
+pwsh -ver
+#If the major version number is lower than 7, follow the instructions to upgrade existing Windows PowerShell. It's important to install the SDK to support .NET tools, as well.
+#You need the .NET SDK installed to run this command.
